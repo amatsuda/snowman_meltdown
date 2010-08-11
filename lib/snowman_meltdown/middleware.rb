29e2a6fd35d234e4caa7d76870ffba672390d894
Akira Matsuda <ronnie@dio.jp>
2010-08-11 10:52:39 +0900

    melt _e as well
    see http://github.com/rails/rails/commit/7171161124a2852ee7b40c5c632ba8e092c97409

--- lib/snowman_meltdown/middleware.rb
+++ lib/snowman_meltdown/middleware.rb
@@ -6,7 +6,7 @@ module SnowmanMeltdown
 
     def call(env)
       status, headers, body = @app.call env
-      body.body = body.body.sub '<input name="_snowman" type="hidden" value="&#9731;" />', '' if body.respond_to? :body
+      body.body = body.body.sub /<input name="(_snowman|_e)" type="hidden" value="&#9731;" \/>/, '' if body.respond_to? :body
       [status, headers, body]
     end
   end
