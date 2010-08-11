521c6be7d037ae6fbfe20eea4cb702436315d54c
Akira Matsuda <ronnie@dio.jp>
2010-08-11 17:52:16 +0900

    tap it!

--- lib/snowman_meltdown/middleware.rb
+++ lib/snowman_meltdown/middleware.rb
@@ -5,9 +5,9 @@ module SnowmanMeltdown
     end
 
     def call(env)
-      status, headers, body = @app.call env
-      body.body = body.body.sub /<input name="(_snowman|_e)" type="hidden" value="&#9731;" \/>/, '' if body.respond_to? :body
-      [status, headers, body]
+      @app.call(env).tap do |status, headers, body|
+        body.body = body.body.sub /<input name="(_snowman|_e)" type="hidden" value="&#9731;" \/>/, '' if body.respond_to? :body
+      end
     end
   end
 end
