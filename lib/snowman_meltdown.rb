ee23f06536405ad1b49658619a1c25eb140b9fc1
Akira Matsuda <ronnie@dio.jp>
2010-08-18 22:53:36 +0900

    Replace the snowmanish tag

--- lib/snowman_meltdown.rb
+++ lib/snowman_meltdown.rb
@@ -1,8 +1,10 @@
+require File.join(File.dirname(__FILE__), 'snowman_meltdown/target_extractor')
 require File.join(File.dirname(__FILE__), 'snowman_meltdown/middleware')
 
 module SnowmanMeltdown
   class Railtie < Rails::Railtie
     initializer 'add middleware' do |app|
+      SnowmanMeltdown::Middleware.snowmanish = TargetExtractor.new.extract
       app.middleware.use SnowmanMeltdown::Middleware
     end
   end
--- lib/snowman_meltdown/middleware.rb
+++ lib/snowman_meltdown/middleware.rb
@@ -1,12 +1,14 @@
 module SnowmanMeltdown
   class Middleware
+    cattr_writer :snowmanish
+
     def initialize(app)
       @app = app
     end
 
     def call(env)
       @app.call(env).tap do |status, headers, body|
-        body.body = body.body.sub /<input name="(_snowman|_e)" type="hidden" value="&#9731;" \/>/, '' if body.respond_to? :body
+        body.body = body.body.sub @@snowmanish, '' if body.respond_to? :body
       end
     end
   end
